7b87a6d9fdce74bb0f03c933935ca3796f8d49b7
Socrates5068 <65322699+Socrates5068@users.noreply.github.com>
2020-09-19 21:49:15 -0400

    RegisterUser

---
+++ app/src/main/java/com/dev/socrates/swing/RegisterUser.java
@@ -0,0 +1,46 @@
+package com.dev.socrates.swing;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterUser extends AppCompatActivity {
+    private RegisterUser root = this;
+    Button cancelButton, registerButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        loadComponents();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void loadComponents() {
+        cancelButton = this.findViewById(R.id.cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivity = new Intent(root, MainActivity.class);
+                root.startActivity(mainActivity);
+            }
+        });
+    }
+}
